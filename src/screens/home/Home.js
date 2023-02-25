410686a4d273447a44716db70c318095520ed444
mandydmello <98489086+mandydmello@users.noreply.github.com>
2023-02-25 07:01:31 +0530

    Styled home page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -16,6 +16,7 @@ import Checkbox from "@material-ui/core/Checkbox";
 import ListItemText from "@material-ui/core/ListItemText";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
+import "./Home.css";
 
 const styles = (theme) => ({
   root: {
